8c59411402251aa02dea0df98d8c27b831036894
azam alias <azamalias@gmail.com>
2015-06-28 07:23:00 +0800

    fixed string concatenation in mas api request

--- malaysiaflights/tests/test_mas.py
+++ malaysiaflights/tests/test_mas.py
@@ -12,9 +12,9 @@ class MASRequestTests(unittest.TestCase):
     def url_helper(self, from_, to, date):
 
         host = 'https://flymh.mobi'
-        path = "/TravelAPI/travelapi/shop/1/mh/"
-        "{from_}/{to}/1/0/0/Economy/{date}/".format(from_=from_,
-                                                    to=to, date=date)
+        path = ("/TravelAPI/travelapi/shop/1/mh/"
+                "{from_}/{to}/1/0/0/Economy/{date}/").format(from_=from_,
+                                                             to=to, date=date)
 
         headers = {'X-apiKey': '52e6d6d613d3a3e825ac02253fe6b5a4',
                    'Accept': 'application/json'}
